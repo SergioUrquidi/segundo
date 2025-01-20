3636da5ae70b3a623ef99eb89ee23344d969e0d7
Sergio Nilo Urquidi Calderon <surquidi@migracion.local>
2025-01-20 18:26:59 -0400

    app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { sequelize } = require('./models');
-require('./src/app.js');
-
 
 const app = express();
 
@@ -11,20 +9,21 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3080;
 async function startServer() {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection established successfully.');
-    await sequelize.sync({ force: false });
-    
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to start server:', error);
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection established successfully.');
+      await sequelize.sync({ force: false });
+      
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error('Unable to start server:', error);
+    }
   }
-}
-
-startServer();
+  
+  startServer();
+  
+  module.exports = app;
