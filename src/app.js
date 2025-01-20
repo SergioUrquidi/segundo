fb50467ce0ed18e097acd9ffae27ea854de041d0
Sergio Nilo Urquidi Calderon <surquidi@migracion.local>
2025-01-20 18:19:00 -0400

    arreglo  index.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { sequelize } = require('./models');
+require('./src/app.js');
+
 
 const app = express();
 
